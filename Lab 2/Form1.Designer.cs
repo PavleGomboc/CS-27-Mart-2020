31558f3f79f7764d1ad85e82835bdca817a96c9e
pgomboc <pgomboc@users.noreply.github.com>
2020-03-27 12:46:51 +0100

    New Commit

--- Lab 2/Form1.Designer.cs
+++ Lab 2/Form1.Designer.cs
@@ -28,20 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.txtUnitPrice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCategoryID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSupplierID = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cboxDisscontinued = new System.Windows.Forms.CheckBox();
+            this.btnInsert = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product Name";
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(94, 10);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(365, 20);
+            this.txtProductName.TabIndex = 1;
+            // 
+            // txtUnitPrice
+            // 
+            this.txtUnitPrice.Location = new System.Drawing.Point(94, 88);
+            this.txtUnitPrice.Name = "txtUnitPrice";
+            this.txtUnitPrice.Size = new System.Drawing.Size(111, 20);
+            this.txtUnitPrice.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Unit Price";
+            // 
+            // txtCategoryID
+            // 
+            this.txtCategoryID.Location = new System.Drawing.Point(94, 62);
+            this.txtCategoryID.Name = "txtCategoryID";
+            this.txtCategoryID.Size = new System.Drawing.Size(111, 20);
+            this.txtCategoryID.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Category ID";
+            // 
+            // txtSupplierID
+            // 
+            this.txtSupplierID.Location = new System.Drawing.Point(94, 36);
+            this.txtSupplierID.Name = "txtSupplierID";
+            this.txtSupplierID.Size = new System.Drawing.Size(111, 20);
+            this.txtSupplierID.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Supplier ID";
+            // 
+            // cboxDisscontinued
+            // 
+            this.cboxDisscontinued.AutoSize = true;
+            this.cboxDisscontinued.Location = new System.Drawing.Point(371, 39);
+            this.cboxDisscontinued.Name = "cboxDisscontinued";
+            this.cboxDisscontinued.Size = new System.Drawing.Size(88, 17);
+            this.cboxDisscontinued.TabIndex = 8;
+            this.cboxDisscontinued.Text = "Discontinued";
+            this.cboxDisscontinued.UseVisualStyleBackColor = true;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(16, 114);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(443, 23);
+            this.btnInsert.TabIndex = 9;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(472, 226);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.cboxDisscontinued);
+            this.Controls.Add(this.txtSupplierID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCategoryID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUnitPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.TextBox txtUnitPrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCategoryID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSupplierID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox cboxDisscontinued;
+        private System.Windows.Forms.Button btnInsert;
     }
 }
 
--- Lab 2/Form1.cs
+++ Lab 2/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace Lab_2
 {
     public partial class Form1 : Form
@@ -15,6 +15,39 @@ namespace Lab_2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            int RetVal = 0; //Save Return value from stored Proc (SQL.txt)
+            SqlConnection conn = new SqlConnection(@"Data Source =.\MSSQLSERVER01; Initial Catalog = TSQL2012; Integrated Security = True; Connection timeout = 5; Application name = Lab 2");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "Production.ProductsInsert";
+            // params
+            cmd.Parameters.Add(new SqlParameter("@RETURN_VALUE", SqlDbType.Int,
+                4, ParameterDirection.ReturnValue, true, 0, 0,"", DataRowVersion.Default, null)); // define ret value
+            SqlParameter PCompName = new SqlParameter("@productname", SqlDbType.NVarChar, 40,
+                ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Default, txtProductName.Text);
+            cmd.Parameters.Add(PCompName);
+
+            cmd.Parameters.AddWithValue("@supplierid", txtSupplierID.Text);
+            cmd.Parameters.AddWithValue("@categoryid", txtCategoryID.Text);
+            cmd.Parameters.AddWithValue("@unitprice", txtUnitPrice.Text);
+            cmd.Parameters.AddWithValue("@discontinued", cboxDisscontinued.Checked);
+
+            try
+            {
+                if (conn.State != ConnectionState.Open) conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                RetVal = (int)cmd.Parameters["@RETURN_VALUE"].Value;
+                MessageBox.Show("Return value: " + RetVal.ToString());
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
